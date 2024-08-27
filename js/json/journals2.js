0621829c10bd71372cbe199678e7f4a25ab4b24a
wcchin <wcchin.88@gmail.com>
2024-08-27 22:58:01 +0800

    change oa

--- js/json/journals2.js
+++ js/json/journals2.js
@@ -3,21 +3,21 @@ journals = [
     "Name": "Annals of the American Association of Geographers",
     "Url": "http://www.tandfonline.com/toc/raag20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "AAG"
   },
   {
     "Name": "Arabian Journal of Geosciences",
     "Url": "https://link.springer.com/journal/12517",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "AJoG"
   },
   {
     "Name": "Computers and Geosciences",
     "Url": "https://www.journals.elsevier.com/computers-and-geosciences",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "CaG"
   },
   {
@@ -25,35 +25,35 @@ journals = [
     "Abbreviation": "CAGIS",
     "Url": "http://www.tandfonline.com/toc/tcag20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "CAGIS"
   },
   {
     "Name": "Computers, Environment and Urban Systems",
     "Url": "https://www.journals.elsevier.com/computers-environment-and-urban-systems/",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "CEaUS"
   },
   {
     "Name": "Canadian Journal of Remote Sensing",
     "Url": "http://www.tandfonline.com/toc/ujrs20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "CJoRS"
   },
   {
     "Name": "Cartographic Perspectives",
     "Url": "http://www.cartographicperspectives.org",
     "Publisher": "NACIS",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "CP"
   },
   {
     "Name": "Cartographica: The International Journal for Geographic Information and Geovisualization",
     "Url": "http://www.utpjournals.press/loi/cart",
     "Publisher": "University of Toronto Press",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "CTIJfGIaG"
   },
   {
@@ -61,7 +61,7 @@ journals = [
     "Abbreviation": "Env Plan B",
     "Url": "http://journals.sagepub.com/home/epb",
     "Publisher": "SAGE",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "EaPBPaD"
   },
   {
@@ -69,35 +69,35 @@ journals = [
     "Abbreviation": "ESIN",
     "Url": "https://link.springer.com/journal/12145",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "ESIN"
   },
   {
     "Name": "Geographical Analysis",
     "Url": "https://onlinelibrary.wiley.com/journal/15384632",
     "Publisher": "Wiley",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "GA"
   },
   {
     "Name": "GIScience & Remote Sensing",
     "Url": "http://www.tandfonline.com/toc/tgrs20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "GaRS"
   },
   {
     "Name": "GeoInformatica",
     "Url": "https://link.springer.com/journal/10707",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "GeoInformatica"
   },
   {
     "Name": "Geomatica",
     "Url": "http://pubs.cig-acsg.ca/journal/cig",
     "Publisher": "CIG",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "Geomatica"
   },
   {
@@ -105,7 +105,7 @@ journals = [
     "Abbreviation": "GSIS",
     "Url": "http://www.tandfonline.com/toc/tgsi20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "GSIS"
   },
   {
@@ -113,7 +113,7 @@ journals = [
     "Abbreviation": "IJDE",
     "Url": "https://www.tandfonline.com/toc/tjde20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJDE"
   },
   {
@@ -121,7 +121,7 @@ journals = [
     "Abbreviation": "IJGGS",
     "Url": "http://www.ipublishing.co.in/jggsindex.html",
     "Publisher": "IPA",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "IJGGS"
   },
   {
@@ -129,7 +129,7 @@ journals = [
     "Abbreviation": "IJGI",
     "Url": "https://www.mdpi.com/journal/ijgi",
     "Publisher": "MDPI",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "IJGI"
   },
   {
@@ -137,34 +137,34 @@ journals = [
     "Abbreviation": "IJGIS",
     "Url": "http://www.tandfonline.com/toc/tgis20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJGIS"
   },
   {
     "Name": "International Journal of Applied Earth Observation and Geoinformation",
     "Url": "https://www.journals.elsevier.com/international-journal-of-applied-earth-observation-and-geoinformation",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJoAEOaG"
   },
   {
     "Name": "International Journal of Geoinformatics",
     "Url": "http://e-geoinfo.net",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJoG"
   },
   {
     "Name": "ISPRS Journal of Photogrammetry and Remote Sensing",
     "Url": "https://www.journals.elsevier.com/isprs-journal-of-photogrammetry-and-remote-sensing/",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJoPaRS"
   },
   {
     "Name": "International Journal of Remote Sensing",
     "Url": "https://www.tandfonline.com/loi/tres20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "IJoRS"
   },
   {
@@ -172,14 +172,14 @@ journals = [
     "Abbreviation": "JGIS",
     "Url": "http://www.scirp.org/journal/jgis/",
     "Publisher": "SCIRP",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "JGIS"
   },
   {
     "Name": "Journal of Geographical Systems",
     "Url": "https://link.springer.com/journal/10109",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "JoGS"
   },
   {
@@ -187,14 +187,14 @@ journals = [
     "Abbreviation": "JGSA",
     "Url": "https://www.springer.com/journal/41651",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "JGSA"
   },
   {
     "Name": "Journal of Location Based Services",
     "Url": "http://www.tandfonline.com/toc/tlbs20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "JoLBS"
   },
   {
@@ -202,20 +202,20 @@ journals = [
     "Abbreviation": "JoM",
     "Url": "http://www.tandfonline.com/toc/tjom20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "JoM"
   },
   {
     "Name": "Journal of Spatial Information Science",
     "Abbreviation": "JOSIS",
     "Url": "http://josis.org/index.php/josis",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "JOSIS"
   },
   {
     "Name": "Journal of Spatial Science",
     "Url": "http://www.tandfonline.com/toc/tjss20/current",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "JoSS"
   },
   {
@@ -223,21 +223,21 @@ journals = [
     "Abbreviation": "KN",
     "Url": "https://link.springer.com/journal/42489",
     "Publisher": "Springer",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "KN"
   },
   {
     "Name": "Open Geospatial Data, Software and Standards",
     "Url": "http://opengeospatialdata.springeropen.com",
     "Publisher": "Springer",
-    "Open Access": true,
+    "Open Access": "yes",
     "Abbr": "OGDSaS"
   },
   {
     "Name": "Photogrammetric Engineering & Remote Sensing",
     "Abbreviation": "PE&RS",
     "Url": "https://www.asprs.org/pers-landing-page",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "PEaRS"
   },
   {
@@ -245,35 +245,35 @@ journals = [
     "Abbreviation": "PFG",
     "Url": "https://link.springer.com/journal/volumesAndIssues/41064",
     "Publisher": "Springer",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "PFG"
   },
   {
     "Name": "Remote Sensing of Environment",
     "Url": "https://www.journals.elsevier.com/remote-sensing-of-environment",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "RSoF"
   },
   {
     "Name": "Spatial Cognition & Computation",
     "Url": "http://www.tandfonline.com/toc/hscc20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "SCaC"
   },
   {
     "Name": "Spatial Statistics",
     "Url": "https://www.journals.elsevier.com/spatial-statistics",
     "Publisher": "Elsevier",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "SS"
   },
   {
     "Name": "The Cartographic Journal",
     "Url": "http://www.tandfonline.com/toc/ycaj20/current",
     "Publisher": "Taylor & Francis",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "TCJ"
   },
   {
@@ -281,7 +281,7 @@ journals = [
     "Abbreviation": "TGIS",
     "Url": "https://onlinelibrary.wiley.com/journal/14679671",
     "Publisher": "Wiley",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "TGIS"
   },
   {
@@ -289,14 +289,14 @@ journals = [
     "Abbreviation": "TGRS",
     "Url": "http://ieeexplore.ieee.org/servlet/opac?punumber=36",
     "Publisher": "IEEE",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "TGRS"
   },
   {
     "Name": "ACM Transactions on Spatial Algorithms and Systems",
     "Url": "https://tsas.acm.org",
     "Publisher": "ACM",
-    "Open Access": false,
+    "Open Access": "",
     "Abbr": "TSAS"
   }
 ]
